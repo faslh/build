58211004cd9713647fd54ac093e720c5a88a8b76
Faslh Generator <generator@faslh.com>
2023-08-02 21:52:55 +0000

    fix syntax

--- src/core/data-source.ts
+++ src/core/data-source.ts
@@ -8,8 +8,8 @@
         url: process.env.CONNECTION_STRING,
         migrationsRun: true,
         entities: [...entites],
-        logging: true // process.env.NODE_ENV !== 'production',
-        synchronize: true // process.env.NODE_ENV !== 'production',
+        logging: true, // process.env.NODE_ENV !== 'production'
+        synchronize: true, // process.env.NODE_ENV !== 'production'
         ssl: process.env.NODE_ENV === 'production',
       };
       export default new DataSource(options);
